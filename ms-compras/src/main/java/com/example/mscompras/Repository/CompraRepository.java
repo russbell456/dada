ba4c596f362091045080bf34951213af6fde7e0c
unknown <andresmorenochechomoreno1995@gmail.com>
2025-04-03 09:24:07 -0500

    delete

--- ms-compras/src/main/java/com/example/mscompras/Repository/CompraRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.mscompras.Repository;
-
-import com.example.mscompras.Entity.Compra;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface CompraRepository extends JpaRepository<Compra, Long> {
-    List<Compra> findByCategoria(String categoria);
-}
