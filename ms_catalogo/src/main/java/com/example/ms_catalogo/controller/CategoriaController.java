35f65964ae8b7b64d42f646273c9ade8e971e3f8
unknown <andresmorenochechomoreno1995@gmail.com>
2025-04-09 10:16:04 -0500

    agregado de gatewa2wy

--- ms_catalogo/src/main/java/com/example/ms_catalogo/controller/CategoriaController.java
+++ ms_catalogo/src/main/java/com/example/ms_catalogo/controller/CategoriaController.java
@@ -14,7 +14,7 @@ public class CategoriaController {
     @Autowired
     private CategoriaService categoriaService;
 
-    @RequestMapping
+    @GetMapping
     public List<Categoria> listar() {
         return categoriaService.listar();
     }
